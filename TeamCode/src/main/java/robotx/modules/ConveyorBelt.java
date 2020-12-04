76ac6e9fa672f6082f425c09195bf31d9301fec0
DESKTOP-E788NN5\Robot-X <braunw11759@stxavier.org>
2020-12-04 15:26:20 -0500

    James created a module for the conveyor belt and added it to the opmode.

---
+++ TeamCode/src/main/java/robotx/modules/ConveyorBelt.java
@@ -0,0 +1,33 @@
+package robotx.modules;
+import robotx.libraries.XModule;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ConveyorBelt extends XModule {
+    DcMotor conveyorMotor;
+
+    double power2 = 0.8;
+
+    public ConveyorBelt(OpMode op) {
+        super(op);
+    }
+
+    public void init() {
+        conveyorMotor = opMode.hardwareMap.dcMotor.get("ConveyorMotor");
+    }
+
+    public void loop() {
+        if (xGamepad2().x.isDown()) {
+            conveyorMotor.setPower(-power2);
+        }
+
+        else if (xGamepad2().y.isDown()) {
+            conveyorMotor.setPower(power2);
+        }
+
+        else {
+            conveyorMotor.setPower(0.0);
+        }
+    }
+}
--- TeamCode/src/main/java/robotx/opmodes/RobotXOpMode2021.java
+++ TeamCode/src/main/java/robotx/opmodes/RobotXOpMode2021.java
@@ -3,26 +3,30 @@ package robotx.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import robotx.libraries.XOpMode;
-import robotx.modules.MecanumDrive;
-//import robotx.modules.IntakeSystem;
+//import robotx.modules.MecanumDrive;
+import robotx.modules.IntakeSystem;
+//import robotx.modules.ConveyorBelt;
 
 @TeleOp(name = "RobotXOpMode2021", group = "Default")
 
 public class RobotXOpMode2021 extends XOpMode {
 
-    MecanumDrive mecanumDrive;
-    //IntakeSystem intakeSystem;
+    //MecanumDrive mecanumDrive;
+    IntakeSystem intakeSystem;
+    //ConveyorBelt conveyorBelt;
 
     public void initModules() {
 
         super.initModules();
 
-        mecanumDrive = new MecanumDrive(this);
-        activeModules.add(mecanumDrive);
+        //mecanumDrive = new MecanumDrive(this);
+        //activeModules.add(mecanumDrive);
 
-        //intakeSystem = new IntakeSystem(this);
-        //activeModules.add(intakeSystem);
+        intakeSystem = new IntakeSystem(this);
+        activeModules.add(intakeSystem);
 
+        //conveyorBelt = new ConveyorBelt(this);
+        //activeModules.add(conveyorBelt);
     }
 
     public void init() {
