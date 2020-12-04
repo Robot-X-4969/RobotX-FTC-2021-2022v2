cb47450c9d83f516c614a9f8ec0074ca2c116aef
DESKTOP-E788NN5\Robot-X <braunw11759@stxavier.org>
2020-12-04 13:32:57 -0500

    Created Autonomous

---
+++ TeamCode/src/main/java/robotx/opmodes/autonomous/RobotXAutonomous2021.java
@@ -0,0 +1,141 @@
+package robotx.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import robotx.libraries.XOpMode;
+import robotx.modules.MecanumDrive;
+import robotx.modules.IntakeSystem;
+
+import static java.lang.Thread.sleep;
+
+@Autonomous(name = "RobotXAutonomous2021", group = "Default")
+
+public class RobotXAutonomous2021 extends LinearOpMode {
+
+    //private ElapsedTime runtime = new ElapsedTime();
+
+    //Modules being imported
+    MecanumDrive mecanumDrive;
+    IntakeSystem intakeSystem;
+
+
+    @Override
+    public void runOpMode() {
+
+        //Text at bottom of phone
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        mecanumDrive = new MecanumDrive(this);
+        mecanumDrive.init();
+
+        intakeSystem = new IntakeSystem(this);
+        intakeSystem.init();
+
+
+        mecanumDrive.start();
+        intakeSystem.start();
+
+        mecanumDrive.frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mecanumDrive.frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mecanumDrive.backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        mecanumDrive.backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData(">", "Press Play to Start Op Mode");
+        telemetry.update();
+
+        waitForStart();
+        //runtime.reset();
+    }
+
+
+        //Controls
+        public void DriveForward(double power, int time) {
+            mecanumDrive.frontLeft.setPower(-power);
+            mecanumDrive.frontRight.setPower(-power);
+            mecanumDrive.backLeft.setPower(-power);
+            mecanumDrive.backRight.setPower(-power);
+            sleep(time);
+            mecanumDrive.frontLeft.setPower(0);
+            mecanumDrive.frontRight.setPower(0);
+            mecanumDrive.backLeft.setPower(0);
+            mecanumDrive.backRight.setPower(0);
+        }
+
+    public void DriveBackward(double power, int time) {
+        mecanumDrive.frontLeft.setPower(power);
+        mecanumDrive.frontRight.setPower(power);
+        mecanumDrive.backLeft.setPower(power);
+        mecanumDrive.backRight.setPower(power);
+        sleep(time);
+        mecanumDrive.frontLeft.setPower(0);
+        mecanumDrive.frontRight.setPower(0);
+        mecanumDrive.backLeft.setPower(0);
+        mecanumDrive.backRight.setPower(0);
+    }
+
+    public void StrafeLeft(double power, int time) {
+        mecanumDrive.frontLeft.setPower(power);
+        mecanumDrive.frontRight.setPower(-power);
+        mecanumDrive.backLeft.setPower(-power);
+        mecanumDrive.backRight.setPower(power);
+        sleep(time);
+        mecanumDrive.frontLeft.setPower(0);
+        mecanumDrive.frontRight.setPower(0);
+        mecanumDrive.backLeft.setPower(0);
+        mecanumDrive.backRight.setPower(0);
+    }
+
+    public void StrafeRight(double power, int time) {
+        mecanumDrive.frontLeft.setPower(-power);
+        mecanumDrive.frontRight.setPower(power);
+        mecanumDrive.backLeft.setPower(power);
+        mecanumDrive.backRight.setPower(-power);
+        sleep(time);
+        mecanumDrive.frontLeft.setPower(0);
+        mecanumDrive.frontRight.setPower(0);
+        mecanumDrive.backLeft.setPower(0);
+        mecanumDrive.backRight.setPower(0);
+    }
+
+    public void TurnLeft(double power, int time) {
+        mecanumDrive.frontLeft.setPower(-power);
+        mecanumDrive.frontRight.setPower(power);
+        mecanumDrive.backLeft.setPower(-power);
+        mecanumDrive.backRight.setPower(power);
+        sleep(time);
+        mecanumDrive.frontLeft.setPower(0);
+        mecanumDrive.frontRight.setPower(0);
+        mecanumDrive.backLeft.setPower(0);
+        mecanumDrive.backRight.setPower(0);
+    }
+
+    public void TurnRight(double power, int time) {
+        mecanumDrive.frontLeft.setPower(power);
+        mecanumDrive.frontRight.setPower(-power);
+        mecanumDrive.backLeft.setPower(power);
+        mecanumDrive.backRight.setPower(-power);
+        sleep(time);
+        mecanumDrive.frontLeft.setPower(0);
+        mecanumDrive.frontRight.setPower(0);
+        mecanumDrive.backLeft.setPower(0);
+        mecanumDrive.backRight.setPower(0);
+    }
+
+    public void StopDriving() {
+        mecanumDrive.frontLeft.setPower(0);
+        mecanumDrive.frontRight.setPower(0);
+        mecanumDrive.backLeft.setPower(0);
+        mecanumDrive.backRight.setPower(0);
+    }
+}
